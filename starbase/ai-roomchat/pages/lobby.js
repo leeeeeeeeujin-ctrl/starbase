8f0b055cbec170a38fcda39c6fe917abc70a137d
leeeeeeeeujin-ctrl <leeeeeeeeujin@gmail.com>
2025-09-20 15:49:56 +0000

    dajsflds

--- starbase/ai-roomchat/pages/lobby.js
+++ starbase/ai-roomchat/pages/lobby.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
-import { supabase } from '../lib/supabase'
+import { supabase } from '../../lib/supabase'
 
 export default function Lobby() {
   const [messages, setMessages] = useState([])
